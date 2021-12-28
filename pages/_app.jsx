6be07ba2006de6fa2e9c55044d8498568750eb46
YauhenDev <buchenkov@gmail.com>
2021-12-28 10:19:33 +0300

    add GTM

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react'
 import Layout from '@components/layouts/Layouts'
+import TagManager from 'react-gtm-module'
 
 import '@styles/app.scss'
+
 import state from 'data/stateJSON.json'
 
+const tagManagerArgs = {
+	gtmId: 'GTM-KHMK3T7'
+}
+
 function Application({ Component, pageProps }) {
+
+	useEffect(() => {
+	
+		//GTM
+		TagManager.initialize(tagManagerArgs);
+
+	},[])
 
 	return (
 		<Layout
