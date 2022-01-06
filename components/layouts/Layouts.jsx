b29497cb25316fa2aa0d34eddcc3a18120984eb0
YauhenDev <buchenkov@gmail.com>
2022-01-06 15:21:57 +0300

    Update GTM strategy - lazyOnload

--- components/layouts/Layouts.jsx
+++ components/layouts/Layouts.jsx
@@ -36,7 +36,8 @@ export default function Layout({
 		</Head>
 
 		<Script
-			strategy="afterInteractive"
+			//strategy="afterInteractive"
+			strategy="lazyOnload"
 			dangerouslySetInnerHTML={{
 				__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
