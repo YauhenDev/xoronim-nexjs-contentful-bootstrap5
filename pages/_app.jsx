5e87d3c98638aec17c26adb3e655e7d41df8c0ab
YauhenDev <buchenkov@gmail.com>
2022-01-07 12:46:42 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -65,7 +65,7 @@ function Application({ Component, pageProps }) {
 
 		</Layout>
 
-		<Script 
+		{/* <Script 
 			strategy="lazyOnload" 
 			src="https://www.googletagmanager.com/gtag/js?id=UA-112416631-3" 
 		/>
@@ -79,7 +79,7 @@ function Application({ Component, pageProps }) {
 					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 					})(window,document,'script','dataLayer','GTM-KHMK3T7');
-				`, */
+				`, * /
 				__html: `
 					window.dataLayer = window.dataLayer || [];
 					function gtag(){dataLayer.push(arguments);}
@@ -89,7 +89,7 @@ function Application({ Component, pageProps }) {
 					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(87015504, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });
 				`,
 			}}
-		/>
+		/> */}
 	</>
 	)
 }
