b346744dd5059ebb59a5bb558cb272c1b12f4117
YauhenDev <buchenkov@gmail.com>
2022-01-07 22:53:18 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -76,11 +76,13 @@ function Application({ Component, pageProps }) {
 				__html: `
 					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
 					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 					})(window,document,'script','dataLayer','GTM-KHMK3T7');
 				`,
 				/* 
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 
+				
 				https://vc.ru/dev/158627-uskoryaem-rabotu-sayta-na-etape-razrabotki-i-obgonyaem-konkurentov-v-poiske-chast-2
 				__html: `
 					window.dataLayer = window.dataLayer || [];
