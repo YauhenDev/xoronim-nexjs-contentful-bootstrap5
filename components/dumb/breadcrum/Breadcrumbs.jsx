5c13f4d9f2297c04b2ffee05f85a574c57eea187
YauhenDev <buchenkov@gmail.com>
2022-02-15 13:50:36 +0300

    BreadCrumbs Schema.org

--- components/dumb/breadcrum/Breadcrumbs.jsx
+++ components/dumb/breadcrum/Breadcrumbs.jsx
@@ -13,10 +13,22 @@ export default function Breadcrumbs({
 
 	function SeconLvl() {
 	return (
-	<Breadcrumb.Item linkAs="span">
+	<Breadcrumb.Item 
+		linkAs="span"
+		itemProp="itemListElement" 
+		itemScope
+		itemType="https://schema.org/ListItem"
+	>
 		<Link href={ scnPathLink }>
-			<a className="bs-0">
-				{ scnNameLink }
+			<a 
+				className="bs-0"
+				itemProp="item"
+			>
+				<span 
+					itemProp="name"
+				>
+					{ scnNameLink }
+				</span>
 			</a>
 		</Link>
 	</Breadcrumb.Item>
@@ -27,14 +39,27 @@ export default function Breadcrumbs({
 		<Row className={`align-items-center ${ styles.row }`}>
 			<Col className="col-12 order-first">
 
-				<Breadcrumb>
+				<Breadcrumb itemScope itemType="https://schema.org/BreadcrumbList">
 
-					<Breadcrumb.Item linkAs="span" className={`bs-0 ${ styles.home }`}>
+					<Breadcrumb.Item 
+						linkAs="span" 
+						className={`bs-0 ${ styles.home }`}
+						itemProp="itemListElement" 
+						itemScope
+      					itemType="https://schema.org/ListItem"
+					>
 						<Link href="/">
 							<a 
 								className="bs-0"
 								title="Ритуальная служба Москвы"
+								itemProp="item"
 							>
+								<span 
+									itemProp="name"
+									className="visually-hidden"
+								>
+									XORONIM.moscow
+								</span>
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
 									<path data-fill="secondary" d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
 								</svg>
@@ -44,13 +69,24 @@ export default function Breadcrumbs({
 
 					{scndLvl ? <SeconLvl />  : ''}
 
-					<Breadcrumb.Item linkAs="span" className={`text-truncate ${styles.active}`}>
+					<Breadcrumb.Item 
+						linkAs="span" 
+						className={`text-truncate ${styles.active}`}
+						itemProp="itemListElement" 
+						itemScope
+      					itemType="https://schema.org/ListItem"
+					>
 						<Link href={ pathLink }>
 							<a 
 								className="bs-0"
 								title={ nameLink }
+								itemProp="item"
 							>
-								{ nameLink }
+								<span 
+									itemProp="name"
+								>
+									{ nameLink }
+								</span>
 							</a>
 						</Link>
 					</Breadcrumb.Item>
