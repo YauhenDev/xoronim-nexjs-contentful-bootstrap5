edbd2e86af4b50c2f72f8f6ae8827bf2995bec71
YauhenDev <buchenkov@gmail.com>
2022-05-30 20:49:34 +0300

    products (fast)

--- pages/ritualnie_prinadlejnosti.jsx
+++ pages/ritualnie_prinadlejnosti.jsx
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
+
+import {Container, Row, Col } from 'react-bootstrap'
 
 import InnerHeader from '@containers/innerHeader/InnerHeader'
 import Breadcrumbs from '@components/dumb/breadcrum/Breadcrumbs'
 import BlockPrice from '@containers/blockPrice/BlockPrice'
+
+//import BlockProduction from '@containers/blockProductions/BlockProductions'
 
 
 import { createClient } from 'contentful'
@@ -38,6 +43,7 @@ export default function Home({
 	const { asPath } = useRouter()
 	const i = 3
 	const { titleLink, seoTitle, seoDescription } = state.mainPages[i]
+	const imgNumber = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 
 	//debugger;
 	return (
@@ -58,6 +64,58 @@ export default function Home({
 			nameLink={ state.mainPages[i].nameLink }
 		/>
 
+		<Container as="section" fluid="xxl">
+			<Row>
+				<Col 
+					xl={ 12 }
+				>
+					<h2>Гробы от 16 000 руб.</h2>
+				</Col>
+			</Row>
+			<Row>
+				{ imgNumber.map( (p, i) => (
+					<Col 
+						key={ i }
+						md={ 6 }
+						lg={ 4 }
+						xl={ 3 }
+					>
+						<Image 
+							src={`/assets/products/groby/${p}.jpg`}
+							alt=""
+							width={300}
+							height={300}
+							className="card-img-top"
+						/>
+					</Col>
+				))}
+			</Row>
+			<Row>
+				<Col 
+					xl={ 12 }
+				>
+					<h2>Венки от 3 000 руб.</h2>
+				</Col>
+			</Row>
+			<Row>
+				{ imgNumber.map( (p, i) => (
+					<Col 
+						key={ i }
+						md={ 6 }
+						lg={ 4 }
+						xl={ 3 }
+					>
+						<Image 
+							src={`/assets/products/venki/${p}.jpg`}
+							alt=""
+							width={300}
+							height={300}
+							className="card-img-top"
+						/>
+					</Col>
+				))}
+			</Row>
+		</Container>
 		
 
 		<BlockPrice 
